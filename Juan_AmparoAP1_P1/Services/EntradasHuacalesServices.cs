74459180b1fe134ef51032d3dc8eea47a6f3ba28
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-02-09 19:59:55 -0400

    servicios de los huacales

--- Juan_AmparoAP1_P1/Services/EntradasHuacalesServices.cs
+++ Juan_AmparoAP1_P1/Services/EntradasHuacalesServices.cs
@@ -14,12 +14,60 @@ namespace Juan_AmparoAP1_P1.Services
             _contexto = contexto;
         }
 
-        public async Task<List<EntradasHuacales>> Listar(Expression<Func<EntradasHuacales, bool>> criterio)
+
+
+        public async Task<bool> Guardar(EntradasHuacales entrada)
+        {
+            if (entrada.idEntrada == 0)
+                return await Insertar(entrada);
+            else
+                return await Modificar(entrada);
+        }
+
+        private async Task<bool> Insertar(EntradasHuacales entrada)
+        {
+            _contexto.EntradasHuacales.Add(entrada);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        private async Task<bool> Modificar(EntradasHuacales entrada)
+        {
+            _contexto.Update(entrada);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var entrada = await _contexto.EntradasHuacales.FindAsync(id);
+            if (entrada != null)
+            {
+                _contexto.EntradasHuacales.Remove(entrada);
+                return await _contexto.SaveChangesAsync() > 0;
+            }
+            return false;
+        }
+
+        public async Task<EntradasHuacales> Buscar(int id)
         {
             return await _contexto.EntradasHuacales
                 .AsNoTracking()
-                .Where(criterio)
+                .FirstOrDefaultAsync(e => e.idEntrada == id);
+        }
+
+        public async Task<List<EntradasHuacales>> ObtenerTodos()
+        {
+            return await _contexto.EntradasHuacales
+                .AsNoTracking()
                 .ToListAsync();
         }
     }
 }
+
+
+
+
+
+
+
+
+
