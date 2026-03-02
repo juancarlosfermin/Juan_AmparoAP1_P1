b42834336d95f6f8ac813be46622ffd8e6d5fbae
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-03-02 11:39:51 -0400

    migraciones

---
+++ Juan_AmparoAP1_P1/Migrations/20260302102006_Inicial1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Juan_AmparoAP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntradassHuacales",
+                columns: table => new
+                {
+                    idEntrada = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NombreCliente = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradassHuacales", x => x.idEntrada);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposHuacales",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Existencia = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposHuacales", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasHuacalesDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntradaId = table.Column<int>(type: "int", nullable: false),
+                    TipoId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasHuacalesDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_EntradasHuacalesDetalle_EntradassHuacales_EntradaId",
+                        column: x => x.EntradaId,
+                        principalTable: "EntradassHuacales",
+                        principalColumn: "idEntrada",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TiposHuacales",
+                columns: new[] { "TipoId", "Descripcion", "Existencia" },
+                values: new object[,]
+                {
+                    { 1, "Huacales Rojos", 0 },
+                    { 2, "Huacales Verdes", 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasHuacalesDetalle_EntradaId",
+                table: "EntradasHuacalesDetalle",
+                column: "EntradaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasHuacalesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "TiposHuacales");
+
+            migrationBuilder.DropTable(
+                name: "EntradassHuacales");
+        }
+    }
+}
